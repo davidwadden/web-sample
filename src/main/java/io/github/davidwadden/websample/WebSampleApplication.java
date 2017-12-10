8a5646c8ee3355c7d5b2ecfbac6ec07b5c618548
David Wadden <dwadden@pivotal.io>
2017-12-09 19:37:59 -0600

    Add SQL Server as runtime dependency
      + Replace whitespace tabs

--- src/main/java/io/github/davidwadden/websample/WebSampleApplication.java
+++ src/main/java/io/github/davidwadden/websample/WebSampleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebSampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebSampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebSampleApplication.class, args);
+    }
 }
